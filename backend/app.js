f1ba94cd47d4363524cb613279fe223d82e037b6
mynameisaditya824@gmail.com <aditya-garg-09-01-2002>
2023-11-27 02:47:31 +0530

    seperate route for otp

--- backend/app.js
+++ backend/app.js
@@ -4,11 +4,14 @@ const app = express();
 const cors=require('cors');
 const loginRoutes=require('./routes/login');
 const registerRoutes=require('./routes/register');
+const otpRoutes =require('./routes/otp');
+
 app.use(cors())
 app.use(express.json())
 
 app.use('/login',loginRoutes);
 app.use('/register',registerRoutes);
+app.use('/send-otp',otpRoutes);
 
 const PORT = process.env.PORT || 3000;
 
