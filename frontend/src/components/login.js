eb90a8146a2164c65fbc548d30a67f402299c990
mynameisaditya824@gmail.com <aditya-garg-09-01-2002>
2023-11-27 01:53:40 +0530

    component for login page

---
+++ frontend/src/components/login.js
@@ -0,0 +1,158 @@
+import React, { useState } from "react";
+import {Link} from "react-router-dom"
+
+export default function Login() {
+  const [userID, setEmail] = useState("");
+  const [userPassword, setPassword] = useState("");
+  const [roomID, setRoomID] = useState("");
+  const handleEmailChange = (e) => {
+    setEmail(e.target.value);
+  };
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleRoomIDChange=(e)=>{
+    setRoomID(e.target.value);
+  }
+
+
+  const handleSubmit = async (e) => {
+    
+    e.preventDefault();
+
+    try {
+      const response = await fetch('http://localhost:3000/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ UserID: userID, UserPassword: userPassword }),
+      });
+
+      const data = await response.json();
+
+      console.log(data);
+
+      // Handle authentication based on the server response
+      if (response.ok) {
+        // Successful login, handle accordingly (e.g., redirect to home page)
+        console.log('Login successful');
+      } else {
+        // Failed login, display error message
+        console.error('Login failed:', data.message);
+      }
+    } catch (error) {
+      console.error('Error during login:', error.message);
+    }
+    setEmail("");
+    setRoomID("");
+    setPassword("");
+  };
+
+  return (
+    <>
+      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img
+            className="mx-auto h-10 w-auto"
+            src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+            alt="Your Company"
+          />
+          {/* image link to anonchat logo */}
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+            Log In To Your Account
+          </h2>
+        </div>
+
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  maxLength={255}
+                  value={userID}
+                  onChange={handleEmailChange}
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                  Password
+                </label>
+                <div className="text-sm">
+                  <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
+                    Forgot password?
+                  </a>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  maxLength={255}
+                  value={userPassword}
+                  onChange={handlePasswordChange}
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="roomID" className="block text-sm font-medium leading-6 text-gray-900">
+                  Room ID
+                </label>
+              </div>
+              <div className="mt-2">
+                <input
+                  id="roomID"
+                  name="roomID"
+                  type="text"
+                  pattern="[A-Za-z0-9]{6}"
+                  maxLength="6"
+                  title="Enter a 6-digit alphanumeric value"
+                  required
+                  value={roomID}
+                  onChange={handleRoomIDChange}
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Log In
+              </button>
+            </div>
+          </form>
+
+          <p className="mt-10 text-center text-sm text-gray-500">
+            Not Joined Yet?{' '}
+            <Link to="/signup" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+              Sign Up
+            </Link>
+          </p>
+        </div>
+      </div>
+    </>
+  );
+}
