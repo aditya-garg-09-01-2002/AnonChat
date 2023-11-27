a9d51039c0480a700b234ab1cbafffaac5964c09
mynameisaditya824@gmail.com <aditya-garg-09-01-2002>
2023-11-27 15:57:37 +0530

    otp verification handled

--- backend/controllers/otpController.js
+++ backend/controllers/otpController.js
@@ -5,19 +5,37 @@ const transporter=require('../utils/mailUtils')
 
 exports.sendOTP=async (req,res)=>{
     const {UserID} = req.body;
-    const otp = Math.floor(100000 + Math.random() * 900000);
+    const otp = Math.floor(100000 + Math.random() * 900000).toString();
+    const hashedOTP =await bcrypt.hash(otp, 10);
+
     try
     {
+        const deleteOldOTP = await dbUtils.query(
+            'Delete from emailverification where UserID=?',
+            [UserID]
+        );
+        const insertNewOTP = await dbUtils.query(
+            'INSERT INTO emailVerification (UserID, otp) VALUES (?, ?)',
+            [UserID, hashedOTP]
+        );
+        
+
         const result = await transporter.sendMail({
             from:"guufg57@gmail.com",
             to: UserID,
             subject: 'Email Verification OTP',
             text: `Your OTP for email verification is: ${otp}`,
         });
-        console.log(result);
+
+        // console.log(result);
+
         if (result.accepted && result.accepted.length > 0) {
             // Email sent successfully
-            res.json({ 'status': 'success', 'message': 'OTP sent successfully' });
+            res.json({
+                 'status': 'success', 
+                 'message': 'OTP sent successfully',
+                 'code':'sent',
+         });
         } else {
             // Email failed to send
             res.status(500).json({ 'status': 'error', 'message': 'Failed to send OTP' });
@@ -29,7 +47,31 @@ exports.sendOTP=async (req,res)=>{
         res.status(500).json({ 'status': 'error', 'message': 'Internal server error' });
     }
 }
-exports.verifyOTP=async(req,res)=>{
+exports.verifyOTP = async (req, res) => {
+    const { UserID, UserOTP } = req.body;
+  try {
+    const rows = await dbUtils.query('SELECT * FROM emailverification WHERE UserID = ?', [UserID]);
 
-}
+    if (rows.length > 0) {
+        const hashedOTP = rows[0].otp;       
+        // Compare the provided password with the hashed password in the database
+        const isOTPValid = await bcrypt.compare( UserOTP,hashedOTP);
+        
+        if (isOTPValid) {
+
+            res.status(200).json({ message: 'OTP Verification Successful' });
+      } else {
+        // Invalid password
+        res.status(401).json({ message: 'Invalid OTP' });
+      }
+    } else {
+      // User not found
+      res.status(404).json({ message: 'Error Not Found' });
+    }
+  } catch (error) {
+    console.error('Error during OTP Verification:', error);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
 
