1abfa2d1ab7121d356b76b1416c360375e3acba8
mynameisaditya824@gmail.com <aditya-garg-09-01-2002>
2023-12-18 03:13:18 +0530

    route from home to school and back, only post method

--- backend/routes/register.js
+++ backend/routes/register.js
@@ -5,5 +5,5 @@ const registerController = require('../controllers/registerController');
 
 // Define user routes
 router.post('/', registerController.addUser);
-
+router.post('/checkUser',registerController.checkUser);
 module.exports = router;
