9ea4daa9d1315a6ff4a4a271f7551fe127223f4a
mynameisaditya824@gmail.com <aditya-garg-09-01-2002>
2023-12-18 03:02:18 +0530

    No change

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,14 +3,16 @@ import './styles.css'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LogIn from './components/login';
 import SignUp from './components/signup';
+import Home from './components/home';
 
 function App() {
   return (
     <div>
       <Router>
         <Routes>
-          <Route path="/" exact element={<LogIn />} />
+          <Route path="/" exact element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
+          <Route path='/home' exact element={<Home />}/>
         </Routes>
       </Router>
     </div>
