3443c0d324687f06050bbb1ec81c563a82e189ca
mynameisaditya824@gmail.com <aditya-garg-09-01-2002>
2023-12-18 03:01:53 +0530

    flowbite plugin added to tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+      "./src/**/*.{html,js}",
+      'node_modules/flowbite-react/lib/esm/**/*.js',
+    ],
   theme: {
     extend: {},
   },
   plugins: [
     require('@tailwindcss/forms'),
+    require('flowbite/plugin'),
   ],
 }
 
