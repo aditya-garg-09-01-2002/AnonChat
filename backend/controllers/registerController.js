a05b4aef268179e1e35e8f56fcd4d6b14926ab59
mynameisaditya824@gmail.com <aditya-garg-09-01-2002>
2023-12-19 21:59:33 +0530

    error messages refined

--- backend/controllers/registerController.js
+++ backend/controllers/registerController.js
@@ -4,57 +4,39 @@ const bcrypt = require('bcrypt');
 // Controller function to get all users
 exports.addUser = async (req, res) => {
     const { UserID, UserName, UserPassword } = req.body;
-    // frontend must send a prop of body with all required attributes
 
-  // Hash the password
   const hashedPassword = await bcrypt.hash(UserPassword, 10);
     try {
         const results = await dbUtils.query(
         'INSERT INTO user (UserID, Password, Name) VALUES (?, ?, ?)',
         [UserID, hashedPassword, UserName]
         );
-        res.json({ success: true, userId: results.insertId });
+        if(results.affectedRows===1)
+            res.status(200).json({ success: true, userId: results.insertId ,message:"User Registered Successfully"});
     } catch (error) {
         if(error.errno==1062)
-        {
-            res.status(500).json({success:false,error:'User Already Exists'});
-        }
-        // console.error('Error inserting user:', error);
+            res.status(400).json({success:false,message:'User Already Exists'});
         else
-        {
-            res.status(500).json({ success: false, error: 'Internal Server Error' });
-        }
+            res.status(500).json({ success: false, message: 'Internal Server Error' });
     }
 };
 exports.checkUser=async(req,res)=>{
     const {UserID}=req.body;
     try {
-        const results = await dbUtils.query(
-        'Select name from User where UserID=?',
-        [UserID]
-        );
-        console.log(results);
+        const results = await dbUtils.query('Select name from User where UserID=?',[UserID]);
         if(results.length>0)
             res.json({'message':'User Already Exists'})
         else 
             res.json({'message':'User Does Not Exists'})
     } catch (error) {
         if(error.errno==1062)
-        {
-            res.status(500).json({success:false,error:'User Already Exists'});
-        }
-        // console.error('Error inserting user:', error);
+            res.json({success:false,error:'User Already Exists'});
         else
-        {
             res.status(500).json({ success: false, error: 'Internal Server Error' });
-        }
     }
 };
 exports.forgotPassword=async(req,res)=>{
     const { UserID, UserPassword } = req.body;
-    // frontend must send a prop of body with all required attributes
-
-  // Hash the password
   const hashedPassword = await bcrypt.hash(UserPassword, 10);
     try {
         const results = await dbUtils.query(
@@ -62,10 +44,9 @@ exports.forgotPassword=async(req,res)=>{
         [hashedPassword, UserID ]
         );
         if(results.affectedRows===1)
-            res.json({ success: true });
-        else res.status(404).json({success:false,error:"User Does Not Exists"})
+            res.status(200).json({ success: true,message:"Password Reset Successfully" });
+        else res.status(404).json({success:false,message:"User Does Not Exists"})
     } catch (error) {
-            console.log(error)
-            res.status(500).json({ success: false, error: 'Internal Server Error' });
+            res.status(500).json({ success: false, message: 'Internal Server Error' });
     }
 }
