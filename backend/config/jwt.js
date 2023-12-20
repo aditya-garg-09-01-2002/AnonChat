cea122980c3245654aec0c8a73d0e3f092086eb7
mynameisaditya824@gmail.com <aditya-garg-09-01-2002>
2023-12-21 02:01:11 +0530

    cookie name

--- backend/config/jwt.js
+++ backend/config/jwt.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const jwtConfig={
-    JWT_SECRET_KEY:process.env.JWT_SECRET_KEY||'your-secret-key'
+    JWT_SECRET_KEY:process.env.JWT_SECRET_KEY||'your-secret-key',
+    JWT_COOKIE_NAME:process.env.JWT_COOKIE_NAME||'AnonChatAuthenticator'
 }
 module.exports=jwtConfig;
