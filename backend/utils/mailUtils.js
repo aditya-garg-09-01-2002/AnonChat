0bd1a2a00cdc0fb63ab4df67dfc0c9c282a80f01
mynameisaditya824@gmail.com <aditya-garg-09-01-2002>
2023-12-20 20:16:03 +0530

    error handling of transport creation

--- backend/utils/mailUtils.js
+++ backend/utils/mailUtils.js
@@ -3,20 +3,28 @@ const mailConfig = require('../config/mailer');
 const nodemailer = require('nodemailer')
 const {google} =require('googleapis')
 
-const oAuth2Client = new google.auth.OAuth2(mailConfig.CLIENT_ID, mailConfig.CLIENT_SECRET, mailConfig.REDIRECT_URI);
-oAuth2Client.setCredentials({refresh_token: mailConfig.REFRESH_TOKEN,});
+let transporter;
 
-const transporter = nodemailer.createTransport({
-    service: mailConfig.service,
-    auth: {
-      type: 'OAuth2',
-      user: mailConfig.auth.user,
-      clientId: mailConfig.CLIENT_ID,
-      clientSecret: mailConfig.CLIENT_SECRET,
-      refreshToken: mailConfig.REFRESH_TOKEN,
-      accessToken: oAuth2Client.getAccessToken(),
-    },
-});
+try{
+  const oAuth2Client = new google.auth.OAuth2(mailConfig.CLIENT_ID, mailConfig.CLIENT_SECRET, mailConfig.REDIRECT_URI);
+  oAuth2Client.setCredentials({refresh_token: mailConfig.REFRESH_TOKEN,});
 
+  transporter = nodemailer.createTransport({
+      service: mailConfig.service,
+      auth: {
+        type: 'OAuth2',
+        user: mailConfig.auth.user,
+        clientId: mailConfig.CLIENT_ID,
+        clientSecret: mailConfig.CLIENT_SECRET,
+        refreshToken: mailConfig.REFRESH_TOKEN,
+        accessToken: oAuth2Client.getAccessToken(),
+      },
+  });
+
+}
+catch(error)
+{
+  throw new Error("Mailing Configuration Error")
+}
 module.exports = transporter;
 
