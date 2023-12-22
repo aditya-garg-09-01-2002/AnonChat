76f90a5fd31aadaaa63e2de7e5cefe9402809981
mynameisaditya824@gmail.com <aditya-garg-09-01-2002>
2023-12-22 23:48:31 +0530

    removed chat routing and refined cors

--- backend/app.js
+++ backend/app.js
@@ -7,7 +7,6 @@ const sessionConfig=require('./config/session')
 const cors=require('cors');
 const loginRoutes=require('./routes/login');
 const registerRoutes=require('./routes/register');
-const chatRoutes=require('./routes/chat');
 const otpRoutes =require('./routes/otp');
 const cookieParser=require('cookie-parser');
 
@@ -16,6 +15,8 @@ const cookieParser=require('cookie-parser');
 app.use(cors({
     origin: [appConfig.FRONTEND_LINK,appConfig.FRONTEND_LINK+'/'],
     credentials: true,
+    allowedHeaders: ["Content-Type", "Authorization", "Access-Control-Allow-Origin", "Access-Control-Request-Headers"],
+    enablePreflight: true
 }));
 
 app.use(cookieParser());
@@ -26,7 +27,6 @@ app.use(express.json())
 app.use('/log',loginRoutes);
 app.use('/register',registerRoutes);
 app.use('/otp',otpRoutes);
-app.use('/chat',chatRoutes);
 
 const PORT = process.env.PORT || 9000;
 
