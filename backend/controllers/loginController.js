7fa74798a496e5f6e3d3068ee95c4627b229b720
mynameisaditya824@gmail.com <aditya-garg-09-01-2002>
2023-12-22 19:55:51 +0530

    added jwt cookie, removed Session cookie, added Role and RoomID in cookie

--- backend/controllers/loginController.js
+++ backend/controllers/loginController.js
@@ -6,7 +6,7 @@ const jwtConfig=require('../config/jwt')
 const sessionConfig=require('../config/session')
 
 exports.logout= (req, res) => {
-  res.clearCookie(sessionConfig.SESSION_COOKIE_NAME);
+  res.clearCookie(jwtConfig.JWT_COOKIE_NAME);
 
   try {
     req.session.destroy((err) => {
@@ -22,8 +22,12 @@ exports.logout= (req, res) => {
 };
 
 
+function getRoomID(UserRole,RoomID){
+  return UserRole==="creator"?"12345678":RoomID
+}
+
 exports.validateLogin = async (req, res) => {
-  const { UserID, UserPassword } = req.body;
+  const { UserID, UserPassword, UserRole, RoomID } = req.body;
   try {
     const rows = await dbUtils.query('SELECT * FROM user WHERE UserID = ?', [UserID]);
     if (rows.length > 0) {
@@ -31,12 +35,22 @@ exports.validateLogin = async (req, res) => {
         // Compare the provided password with the hashed password in the database
         const isPasswordValid = await bcrypt.compare(UserPassword, hashedPassword);
         if (isPasswordValid) {
-
-          const jwtToken=jwt.sign({User:UserID},jwtConfig.JWT_SECRET_KEY,{expiresIn:'1d',});
-            req.session.user={
-              UserID:UserID,
-              jwtToken
-            }
+          console.log(rows[0])
+        const jwtToken=jwt.sign({
+          UserID,
+          UserRole,
+          RoomID:getRoomID(UserRole,RoomID),
+          UserName:rows[0].name
+        },jwtConfig.JWT_SECRET_KEY,{expiresIn:'2h',});
+          res.cookie(jwtConfig.JWT_COOKIE_NAME,jwtToken,{
+            secret: jwtConfig.JWT_SECRET_KEY,
+            resave: false,
+            saveUninitialized: false,
+            maxAge: 1000*59*60*2, // 1 hour 59 min authentication only
+            cookie: { 
+                secure: jwtConfig.ENVIRONMENT==="production"?true:false,
+                sameSite:'none', //production build must have this set
+            }})
             res.status(200).json({ message: 'Login Successful'});
       } else {
         // Invalid password
@@ -47,7 +61,6 @@ exports.validateLogin = async (req, res) => {
       res.status(404).json({ message: 'User Not Found' });
     }
   } catch (error) {
-    console.log(error)
     res.status(500).json({ message: error.message });
   }
 };
