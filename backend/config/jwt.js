88a12567bf7d4627395e5c1a804612e441c23827
mynameisaditya824@gmail.com <aditya-garg-09-01-2002>
2023-12-22 23:30:26 +0530

    updated configurations

--- backend/config/jwt.js
+++ backend/config/jwt.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const jwtConfig={
     JWT_SECRET_KEY:process.env.JWT_SECRET_KEY||'your-secret-key',
-    JWT_COOKIE_NAME:process.env.JWT_COOKIE_NAME||'AnonChatAuthenticator'
+    JWT_COOKIE_NAME:process.env.JWT_COOKIE_NAME||'AnonChatAuthenticator',
+    ENVIRONMENT:process.env.ENVIRONMENT||'development',
 }
 module.exports=jwtConfig;
