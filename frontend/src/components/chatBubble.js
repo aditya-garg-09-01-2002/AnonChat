07d978b2a6df8f71d490bb5a07f84ed4611950ed
mynameisaditya824@gmail.com <aditya-garg-09-01-2002>
2023-12-23 23:29:24 +0530

    bubble Props: message:string, sent:boolean

---
+++ frontend/src/components/chatBubble.js
@@ -0,0 +1,28 @@
+import {React} from "react"
+
+export default function Bubble({message,sent}){
+    return (
+        <>
+            <div class="bubbleContainer" style={{
+                marginBottom:"10px",
+                marginRight:sent?"0":"auto",
+                marginLeft:sent?"auto":"0",
+                backgroundColor:"white",
+                padding:"4px 10px",
+                border:"green solid 2px",
+                borderRadius:"10px",
+                maxWidth:"80%",
+                wordBreak:"break-word",
+            }}>
+                <div class="bubble" style={{
+                    padding:"0px",
+                    margin:"0px",
+                    color:sent===true?"purple":"green",
+                    textWrap:"wrap",
+                }}>
+                    {message}
+                </div>
+            </div>
+        </>
+    )
+}
