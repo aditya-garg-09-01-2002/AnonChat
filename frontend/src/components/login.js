65480c58cafa29e630b21b8c2b3b03b2f9317d2b
mynameisaditya824@gmail.com <aditya-garg-09-01-2002>
2023-12-25 23:46:51 +0530

    Input Components Used

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,15 +1,30 @@
 import React, { useState ,useEffect} from "react";
 import {Link , useNavigate} from "react-router-dom"
 import MessagePop from "./messagePop";
+import { PasswordInput,EmailInput } from "./inputs";
+// import Handlers from "./utils/inputHandlers";
 
 
 export default function Login() {
   const navigate=useNavigate();
-  const [userID, setEmail] = useState("");
+  const [userRole,setRole]=useState("joinee");const [userID, setEmail] = useState("");
   const [userPassword, setPassword] = useState("");
-  const [userRole,setRole]=useState("joinee");
-  const [roomID,setRoomID]=useState("")
-
+  const [roomID,setRoomID]=useState("");
+  
+  
+  // const {userID,userPassword,roomID,handleRoomIDChange,handleEmailChange,handlePasswordChange}=Handlers()
+  
+  const handleRoomIDChange=(e)=>{
+      setRoomID(e.target.value)
+  };
+  
+  const handleEmailChange = (e) => {
+      setEmail(e.target.value);
+  };
+  
+  const handlePasswordChange = (e) => {
+      setPassword(e.target.value);
+  };
   
   function clearFields(){
     setEmail("");
@@ -20,19 +35,6 @@ export default function Login() {
   
   const [modalProps,setShowModal]=useState({modalOpen:false,modalMessage:"",modalButtons:[{name:"",color:"",link:""}],clearFields:clearFields,onOTP:false,modalStatus:""})
   
-  const handleRoomIDChange=(e)=>{
-    setRoomID(e.target.value)
-  }
-
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
-
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
-
-
   useEffect(()=>{
     (async()=>{
       try{
@@ -104,50 +106,11 @@ export default function Login() {
 
         <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
           <form className="space-y-6" onSubmit={handleSubmit}>
-            <div>
-              <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
-                Email address
-              </label>
-              <div className="mt-2">
-                <input
-                  id="email"
-                  name="email"
-                  type="email"
-                  autoComplete="email"
-                  required
-                  maxLength={255}
-                  value={userID}
-                  onChange={handleEmailChange}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
+            <EmailInput userID={userID} registrationButton={0} handleEmailChange={handleEmailChange}/>
 
-            <div>
-              <div className="flex items-center justify-between">
-                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
-                  Password
-                </label>
-                <div className="text-sm">
-                <Link to='/reset' className="font-semibold text-indigo-600 hover:text-indigo-500">
-                  Forgot Password
-                </Link>
-                </div>
-              </div>
-              <div className="mt-2">
-                <input
-                  id="password"
-                  name="password"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  maxLength={255}
-                  value={userPassword}
-                  onChange={handlePasswordChange}
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
+
+            <PasswordInput userPassword={userPassword} registrationButton={0} handlePasswordChange={handlePasswordChange} title={"Password"} />
+
 
             <div>
               <button className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
