3c1030a91b94dfc2739a840c0cf3540201e9af8d
mynameisaditya824@gmail.com <aditya-garg-09-01-2002>
2023-12-25 16:17:53 +0530

    all chat bubbles are rendered

---
+++ frontend/src/components/chat/chatContainer.js
@@ -0,0 +1,33 @@
+import {React, useEffect,useRef} from "react"
+import ChatBubble from "./chatBubble"
+
+export default function ChatContainer({chats,lineHeight,height,margin}){
+    const chatContainerRef=useRef(null);
+    useEffect(()=>{
+        chatContainerRef.current.scrollTo({
+            top:chatContainerRef.current.scrollHeight,
+            behavior:"smooth",
+        });
+    },[chats])
+    return (
+        <>
+            <div 
+                ref={chatContainerRef}
+                id="chatContainer" style={{
+                lineHeight:lineHeight,
+                boxSizing:"border-box",
+                height:height,
+                border:"none",
+                margin:margin,
+                width:"100%",
+                display:"flex",
+                flexDirection:"column",
+                overflowY:"auto",
+            }}>
+                {chats.map((chat,index)=>(
+                    <ChatBubble first={index===0} key={index} message={chat.message} time={chat.time} sent={chat.sent} />
+                ))}
+            </div>
+        </>
+    )
+}
