cdac24b30d032aba66129775822f23b184da83dd
mynameisaditya824@gmail.com <aditya-garg-09-01-2002>
2023-12-26 00:27:35 +0530

    name input component

--- frontend/src/components/inputs/index.js
+++ frontend/src/components/inputs/index.js
@@ -2,3 +2,4 @@ export {default as EmailInput} from "./email";
 export {default as PasswordInput} from "./userPassword";
 export {default as OTPInput} from "./otp"
 export {default as RoomID} from "./roomID";
+export {default as NameInput} from "./name";
