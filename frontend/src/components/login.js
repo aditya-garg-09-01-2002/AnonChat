42f3669540b9814a1a2b1dc7f0da0ffc527ed3cc
mynameisaditya824@gmail.com <aditya-garg-09-01-2002>
2023-12-25 21:40:18 +0530

    reverted file paths

--- frontend/src/components/login.js
+++ frontend/src/components/login.js
@@ -1,6 +1,6 @@
 import React, { useState ,useEffect} from "react";
 import {Link , useNavigate} from "react-router-dom"
-import MessagePop from "../messagePop";
+import MessagePop from "./messagePop";
 
 
 export default function Login() {
