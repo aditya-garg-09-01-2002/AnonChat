6366f9fa2c61edb35079dec6b85efae18b4c96eb
mynameisaditya824@gmail.com <aditya-garg-09-01-2002>
2023-12-28 02:36:41 +0530

    created socket server on express server

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,15 @@
 const express = require('express');
 const app = express();
 const appConfig=require('./config/appConfig')
-const jwtConfig=require('./config/jwt')
-const sessionConfig=require('./config/session')
 const cors=require('cors');
 const loginRoutes=require('./routes/login');
 const registerRoutes=require('./routes/register');
+const chatRoutes=require('./routes/chat');
 const otpRoutes =require('./routes/otp');
 const cookieParser=require('cookie-parser');
+const socketIO=require('socket.io');
+const {getJWT,parseCookies}=require('./utils/jwtUtils')
+const {getSize,curTime}=require('./utils/chatUtils')
 
 
 
@@ -18,16 +20,25 @@ app.use(cors({
 }));
 
 app.use(cookieParser());
-    
+
 app.use(express.json())
 
 
 app.use('/log',loginRoutes);
 app.use('/register',registerRoutes);
 app.use('/otp',otpRoutes);
+app.use('/chat',chatRoutes);
 
 const PORT = process.env.PORT || 9000;
 
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
+});
+
+const io = socketIO(server, {
+    pingTimeout: 60000,
+    cors: {
+        origin: [appConfig.FRONTEND_LINK,appConfig.FRONTEND_LINK+'/'],
+        credentials:true,
+    },
 });
