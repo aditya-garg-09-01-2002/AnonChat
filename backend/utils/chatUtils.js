a2db1f2cf2a0f4bd8c8e1bb42ab2c769a460320c
mynameisaditya824@gmail.com <aditya-garg-09-01-2002>
2023-12-28 02:32:40 +0530

    function for current time

--- backend/utils/chatUtils.js
+++ backend/utils/chatUtils.js
@@ -1,5 +1,11 @@
 const getSize=(io,roomID)=>{
     return io.sockets.adapter.rooms.get(roomID)?.size||0;
 }
+const curTime=()=>{
+    const date=new Date();
+    const hours=date.getHours();
+    const minutes=date.getMinutes();
+    return (hours>9?hours:hours>0?"0"+hours:"00")+":"+(minutes>9?minutes:minutes>0?"0"+minutes:"00")
+} 
 
-module.exports={getSize}
+module.exports={getSize,curTime}
