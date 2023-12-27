b62b6a49f75f64300a63934fdf0cb1eed8b5a11b
mynameisaditya824@gmail.com <aditya-garg-09-01-2002>
2023-12-28 02:23:23 +0530

    authorization while chatting

--- backend/utils/jwtUtils.js
+++ backend/utils/jwtUtils.js
@@ -3,9 +3,11 @@ const jwtConfig=require('../config/jwt');
 
 const getJWT=(allCookies)=>{
     const encoded_jwtToken=allCookies[jwtConfig.JWT_COOKIE_NAME];
-    console.log(encoded_jwtToken);
+    if(!encoded_jwtToken)
+        return {authorized:false};
     const decoded_jwtToken=jwt.verify(encoded_jwtToken,jwtConfig.JWT_SECRET_KEY)
     return {
+        authorized:true,
         userName:decoded_jwtToken.UserName,
         userRole:decoded_jwtToken.UserRole,
         userName:decoded_jwtToken.UserName,
