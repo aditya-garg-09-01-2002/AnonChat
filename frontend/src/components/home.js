6a61021140c35d942a4d18e5a53334915e5208c3
mynameisaditya824@gmail.com <aditya-garg-09-01-2002>
2023-12-28 02:31:51 +0530

    corrected route for logout

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -58,16 +58,16 @@ export default function Home(){
         socketRef.current=io(process.env.REACT_APP_BACKEND_LINK,{withCredentials:true});
         socketRef.current.emit('join',socketRef.current.id)
         socketRef.current.on('room-unavailable',()=>{
-            setShowModal({modalOpen:true,modalMessage:"Room is not Created",modalButtons:[{name:"Either Create or Join a Different Room",color:"failure",link:"_logout"}],modalStatus:"sad"});
+            setShowModal({modalOpen:true,modalMessage:"Room is not Created",modalButtons:[{name:"Either Create or Join a Different Room",color:"failure",link:"_logout_"}],modalStatus:"sad"});
         })
         socketRef.current.on('permission-denied',()=>{
-            setShowModal({modalOpen:true,modalMessage:"Fatal Authorization Error",modalButtons:[{name:"Try Logging in Again",color:"failure",link:"_logout"}],modalStatus:"sad"});
+            setShowModal({modalOpen:true,modalMessage:"Fatal Authorization Error",modalButtons:[{name:"Try Logging in Again",color:"failure",link:"_logout_"}],modalStatus:"sad"});
         })
         socketRef.current.on('receive-message',(received)=>
             setChat((prevChats)=>[...prevChats,{message:received.message,sent:false,time:received.time}])
         )
         socketRef.current.on('session-expired',()=>{
-            setShowModal({modalOpen:true,modalMessage:"Fatal Authorization Error",modalButtons:[{name:"Try Logging in Again",color:"failure",link:"_logout"}],modalStatus:"sad"});
+            setShowModal({modalOpen:true,modalMessage:"Fatal Authorization Error",modalButtons:[{name:"Try Logging in Again",color:"failure",link:"_logout_"}],modalStatus:"sad"});
         })
         return ()=>{
             socketRef.current.emit('leave')
