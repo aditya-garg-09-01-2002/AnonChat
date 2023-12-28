ec5706950c6ece7606cb2f9d1b195830d7da9e5e
mynameisaditya824@gmail.com <aditya-garg-09-01-2002>
2023-12-28 23:49:25 +0530

    updated getRoomID

--- backend/utils/chatUtils.js
+++ backend/utils/chatUtils.js
@@ -1,3 +1,5 @@
+const crypto=require('crypto')
+
 const getSize=(io,roomID)=>{
     return io.sockets.adapter.rooms.get(roomID)?.size||0;
 }
@@ -8,4 +10,12 @@ const curTime=()=>{
     return (hours>9?hours:hours>0?"0"+hours:"00")+":"+(minutes>9?minutes:minutes>0?"0"+minutes:"00")
 } 
 
-module.exports={getSize,curTime}
+const getRoomID=(UserRole,RoomID,UserName)=>{
+    if(UserRole==="creator")
+    {
+        return crypto.randomBytes(8).toString('hex');
+    }
+    return RoomID;
+}
+
+module.exports={getSize,curTime,getRoomID}
