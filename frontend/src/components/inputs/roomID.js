42578e88f3d35d47ae18dd42df237a5c47e3d792
mynameisaditya824@gmail.com <aditya-garg-09-01-2002>
2024-01-01 17:01:59 +0530

    updated layout for RoomID input and button

--- frontend/src/components/inputs/roomID.js
+++ frontend/src/components/inputs/roomID.js
@@ -1,48 +1,41 @@
-import React,{forwardRef, useEffect} from "react";
+import React,{forwardRef} from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowTurnUp } from '@fortawesome/free-solid-svg-icons';
 
 const RoomID=forwardRef(({userRole,roomID,handleRoomIDChange,setRole},roomButtonRef)=>{
-    useEffect(()=>{
-        console.log(roomButtonRef)
-    },[roomButtonRef])
     return(
         <>
-            <div style={{display:"flex",alignItems:"center",}}>
-                <div style={{textWrap:"nowrap"}}>
-                    I want to
+            <div>
+                <div className="flex items-center justify-between">
+                    <button className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    ref={roomButtonRef}
+                    onClick={(e)=>{
+                        e.preventDefault()
+                        userRole==="joinee"?setRole("creator"):setRole("joinee")
+                    }}>
+                        {userRole==="creator"?"Create a Room":"Join a Room"}
+                    </button>
                 </div>
-                <button className="flex mx-2 justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus:outline focus:outline-2 focus:outline-offset-2 focus-visible:outline-indigo-600"
-                ref={roomButtonRef}
-                onClick={(e)=>{
-                    e.preventDefault()
-                    userRole==="joinee"?setRole("creator"):setRole("joinee")
-                }}>
-                    {userRole==="creator"?"create":"join"}
-                </button>
-                <div style={{textWrap:"nowrap"}}>
-                    a room
-                </div>  
-                <div className="mx-2">
-                    <div className="">
-                        <input
-                            id="roomid"
-                            name="roomid"
-                            type="text"
-                            autoComplete="off"
-                            required
-                            maxLength={16}
-                            value={userRole==="creator"?"N/A":roomID}
-                            onChange={handleRoomIDChange}
-                            disabled={userRole==="creator"}
-                            className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                        />
-                    </div>
-                </div>
-            </div>
-            <div className="w-full" style={{textAlign:"center"}}>
-                {userRole==="creator"?"A new room will be created with random Room ID":`You will be Joining Room ${roomID==="Enter Room ID"?"N/A":""}`}
-                {userRole==="joinee"?<FontAwesomeIcon icon={faArrowTurnUp} style={{color:"black",transform:"scale(2))",paddingLeft:"3px"}}/>:""}
+                {
+                    userRole==="joinee"?
+                        <div className="mt-6">
+                            <input
+                                id="roomid"
+                                name="roomid"
+                                type="text"
+                                autoComplete="off"
+                                pattern=""
+                                required="[0-9]{16}"
+                                maxLength={16}
+                                value={roomID}
+                                onChange={handleRoomIDChange}
+                                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 " style={{textAlign:"center"}}
+                            />
+                        </div>:
+                        <div className="block w-full rounded-md border-0 py-1.5 text-gray-500 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6 mt-6" style={{textAlign:"center"}}>
+                            A new room will be created with random Room ID
+                        </div>
+                }
             </div>
         </>
     )
