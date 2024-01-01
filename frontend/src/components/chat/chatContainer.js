c4842ecf6d1ea10f2e694f7be1c232c41a7b0720
mynameisaditya824@gmail.com <aditya-garg-09-01-2002>
2024-01-02 00:41:20 +0530

    indentation

--- frontend/src/components/chat/chatContainer.js
+++ frontend/src/components/chat/chatContainer.js
@@ -2,7 +2,7 @@ import {React, useEffect,useRef} from "react"
 import ChatBubble from "./chatBubble"
 
 export default function ChatContainer({chats,size}){
-    const {lineHeight,height,margin}=size;
+    const {lineHeight,height,margin,padding}=size;
     const chatContainerRef=useRef(null);
     useEffect(()=>{
         chatContainerRef.current.scrollTo({
@@ -15,16 +15,18 @@ export default function ChatContainer({chats,size}){
             <div 
                 ref={chatContainerRef}
                 id="chatContainer" style={{
-                lineHeight:lineHeight,
-                boxSizing:"border-box",
-                height:height,
-                border:"none",
-                margin:margin,
-                width:"100%",
-                display:"flex",
-                flexDirection:"column",
-                overflowY:"auto",
-            }}>
+                    padding:padding,
+                    lineHeight:lineHeight,
+                    boxSizing:"border-box",
+                    height:height,
+                    border:"none",
+                    margin:margin,
+                    width:"100%",
+                    display:"flex",
+                    flexDirection:"column",
+                    overflowY:"auto",
+                }}
+            >
                 {chats.map((chat,index)=>(
                     <ChatBubble first={index===0} key={index} message={chat.message} time={chat.time} sent={chat.sent} />
                 ))}
